f7c2698fd9713a55e895f82d590ed75e6e3206a9
MitasovArthur <mitasovartur2@gmail.com>
2023-02-26 00:34:06 +0200

    add sout

--- src/main/java/OOp/main.java
+++ src/main/java/OOp/main.java
@@ -3,6 +3,7 @@ package OOp;
 public class main {
     public static void main(String[] args) {
         System.err.print("Not Error");
+        System.out.println("2_1 lab Mitasov Arthur");
 
     }
 }
